60fa9fcc11eb32c5b9d953621adac0ad5c24053d
Mesh-254 <meshack1995@gmail.com>
2023-10-19 12:44:43 +0300

    created recipes model

---
+++ recipe-project/models/recipe.py
@@ -0,0 +1,18 @@
+from .database import db
+from flask_login import UserMixin
+
+
+class Recipe(db.Model, UserMixin):
+    """recipes model for the project"""
+    __tablename__ = 'recipes'
+    recipe_id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(255), nullable=False)
+    description = db.Column(db.Text)
+    source_url = db.Column(db.String(255))
+    image_url = db.Column(db.String(255))
+    spoonacular_id = db.Column(db.Integer, unique=True, nullable=False)
+    comments = db.relationship('Comment', backref='recipe', lazy=True)
+    ratings = db.relationship('Rating', backref='recipe', lazy=True)
+
+    def __repr__(self):
+        return vars(Recipe)
