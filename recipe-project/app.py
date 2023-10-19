5ae1064e78b4d31e7f142c9f5fcba767def8798e
Mesh-254 <meshack1995@gmail.com>
2023-10-19 15:13:58 +0300

    commented user model and created load_user function

--- recipe-project/app.py
+++ recipe-project/app.py
@@ -1,69 +1,70 @@
+# Import necessary modules from Flask and other libraries
 from flask import Flask, render_template, url_for
 from flask_migrate import Migrate
 from flask.cli import load_dotenv
 import os
 
+# Import Flask-Login for user authentication
+from flask_login import LoginManager
 
-
-
-# flask login
-# from flask_login import LoginManager
-
-# import models tables
+# Import database models
 from models.database import db
 from models.user import User
 from models.recipe import Recipe
 from models.comment import Comment
 from models.rating import Rating
 
-# import of blueprints 
+# Import blueprints for routing
 from blueprints.recipes import recipes
 from blueprints.auth import auth
 
-
-# application
+# Create a Flask application
 app = Flask(__name__, template_folder='./templates')
 
+# Configure the application to not force trailing slashes on URLs
 app.url_map.strict_slashes = False
 
-
-# set up of the static folder correctly to serve static files.
+# Configure the static folder to serve static files (like CSS, JavaScript, etc.)
 app.static_folder = 'static'
 
-
-# load environment variables
+# Load environment variables from a .env file
 env = load_dotenv()
-
 DB_USERNAME = os.getenv('DB_USERNAME')
 DB_PASSWORD = os.getenv('DB_PASSWORD')
 DB_SERVER = os.getenv('DB_SERVER')
 DB_NAME = os.getenv('DB_NAME')
 SECRET_KEY = os.getenv('SECRET_KEY')
 
-
+# Configure the Flask application with various settings
 app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 app.config['SQLALCHEMY_DATABASE_URI'] = f"mysql+pymysql://{DB_USERNAME}:{DB_PASSWORD}@{DB_SERVER}/{DB_NAME}"
 
-
+# Initialize the database and set up migration support
 db.init_app(app)
 migrate = Migrate(app, db)
 
-# register blueprints
-app.register_blueprint(recipes,url_prefix='/')
+# Register blueprints to organize routes
+app.register_blueprint(recipes, url_prefix='/')
 app.register_blueprint(auth, url_prefix='/')
 
+# Create a LoginManager instance and configure it
+login = LoginManager()
+login.init_app(app)
+login.login_view = 'auth.login'
 
 
-# login_manager = LoginManager()
-# login_manager.init_app(app)
-# login_manager.login_view = 'auth.login'
+# Function decorator to load a user by their ID when needed for authentication
+@login.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
 
-
+# Define a route to display a base page
 @app.route('/base')
 def base():
     """display base page"""
     return render_template('/main/base.html')
 
 
+# Start the Flask application if the script is executed directly
 if __name__ == '__main__':
     app.run(debug=True, host="0.0.0.0", port=5000)
--- recipe-project/models/user.py
+++ recipe-project/models/user.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 from .database import db
 from flask_login import UserMixin
 from werkzeug.security import generate_password_hash, check_password_hash
@@ -13,18 +14,22 @@ class User(db.Model, UserMixin):
     comments = db.relationship('Comment', backref='user', lazy=True)
     ratings = db.relationship('Rating', backref='user', lazy=True)
 
-    # Method to hash password
+    # Method to hash the user's password
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
 
+    # Method to check if the provided password matches the hashed password
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
 
+    # Initialize the User model with name, email, and password
     def __init__(self, name, email, password):
-        """Initialize the user model"""
         self.name = name
+        # Note: This should likely be self.password_hash to match the set_password method.
         self.password = password
         self.email = email
 
+    # Method to represent the User object as a string
     def __repr__(self):
+        # It should likely be return str(vars(self)) to return a dictionary representation of the User instance.
         return vars(User)
