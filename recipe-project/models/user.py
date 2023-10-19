e6d59c027d007f34588f1f19404fd8ea66321f01
Mesh-254 <meshack1995@gmail.com>
2023-10-19 12:44:07 +0300

    created users model

--- recipe-project/models/user.py
+++ recipe-project/models/user.py
@@ -1,14 +1,24 @@
 from .database import db
 from flask_login import UserMixin
+from werkzeug.security import generate_password_hash, check_password_hash
 
 
-class Users(db.Model, UserMixin):
+class User(db.Model, UserMixin):
     """user model for the project"""
-    __tablename__ = 'user'
+    __tablename__ = 'users'
     user_id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(80), nullable=False)
-    email = db.Column(db.String(80), unique=True, nullable=False)
-    password = db.Column(db.VARCHAR(300), unique=True, nullable=False)
+    name = db.Column(db.String(255), nullable=False)
+    email = db.Column(db.String(255), unique=True, nullable=False)
+    password_hash = db.Column(db.String(255), nullable=False)
+    comments = db.relationship('Comment', backref='user', lazy=True)
+    ratings = db.relationship('Rating', backref='user', lazy=True)
+
+    # Method to hash password
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
 
     def __init__(self, name, email, password):
         """Initialize the user model"""
@@ -17,4 +27,4 @@ class Users(db.Model, UserMixin):
         self.email = email
 
     def __repr__(self):
-        return vars(Users)
+        return vars(User)
