2bf65a3c0aa7564aa31853a7b7f000224efbceac
Mesh-254 <meshack1995@gmail.com>
2023-10-19 12:43:17 +0300

    created comment model

---
+++ recipe-project/models/comment.py
@@ -0,0 +1,16 @@
+from .database import db
+from flask_login import UserMixin
+
+
+class Comment(db.Model, UserMixin):
+    """comments model for the project"""
+
+    __tablename__ = 'comments'
+    comment_id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey(
+        'users.user_id'), nullable=False)
+    recipe_id = db.Column(db.Integer, db.ForeignKey(
+        'recipes.recipe_id'), nullable=False)
+    comment_text = db.Column(db.Text, nullable=False)
+    comment_date = db.Column(
+        db.TIMESTAMP, server_default=db.func.current_timestamp())
