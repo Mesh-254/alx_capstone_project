25e209f5201c2defc7685ae93c4a18048d04e595
Mesh-254 <meshack1995@gmail.com>
2023-10-20 15:55:23 +0300

    created route to display user profile

--- recipe-project/blueprints/user.py
+++ recipe-project/blueprints/user.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 """Module to display User information """
+from datetime import datetime
 from flask import *
-from flask_login import login_required
+import pytz
+from flask_login import login_required, current_user
 
 
 from models.user import User
@@ -10,13 +12,28 @@ from models.database import db
 
 user = Blueprint('user', __name__)
 
-@app.route('/user/<email>')
+
+@user.route('/user/<email>')
 @login_required
-def user(email):
+def profile(email):
     user = User.query.filter_by(email=email).first_or_404()
     user_info = [
-        {'Name': user.name},
-        {"Email": user.email}
+        {"Name": user.name},
+        {"Email": user.email},
     ]
+    return render_template('user/profile.html', user=user, user_info=user_info)
 
-    return render_template('')
+
+@user.before_request
+def before_request():
+    # This function is executed before each request to the application
+
+    if current_user.is_authenticated:
+        # Check if the current user is authenticated
+
+        # Update the 'last_seen' attribute for the authenticated user
+        east_african_timezone = pytz.timezone('Africa/Nairobi')
+        current_user.last_seen = datetime.now(east_african_timezone)
+
+        # Commit the changes to the database
+        db.session.commit()
