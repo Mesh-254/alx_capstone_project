760d794ed05494c9e46df3519473c294f25d5509
Mesh-254 <meshack1995@gmail.com>
2023-10-20 07:20:21 +0300

    updated user model function to get user id

--- recipe-project/models/user.py
+++ recipe-project/models/user.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 from .database import db
 from flask_login import UserMixin
-from werkzeug.security import generate_password_hash, check_password_hash
+
 
 
 class User(db.Model, UserMixin):
@@ -10,19 +10,15 @@ class User(db.Model, UserMixin):
     user_id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(255), nullable=False)
     email = db.Column(db.String(255), unique=True, nullable=False)
-    password_hash = db.Column(db.String(255), nullable=False)
+    password = db.Column(db.VARCHAR(300), nullable=False)
     comments = db.relationship('Comment', backref='user', lazy=True)
     ratings = db.relationship('Rating', backref='user', lazy=True)
 
-    # Method to hash the user's password
-    def set_password(self, password):
-        self.password_hash = generate_password_hash(password)
-
-    # Method to check if the provided password matches the hashed password
-    def check_password(self, password):
-        return check_password_hash(self.password_hash, password)
+    def get_id(self):
+        return str(self.user_id) 
 
     # Initialize the User model with name, email, and password
+
     def __init__(self, name, email, password):
         self.name = name
         # Note: This should likely be self.password_hash to match the set_password method.
