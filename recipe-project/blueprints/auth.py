41c143aa74bbf3329e0a49b02a50f5f427ba1c79
Mesh-254 <meshack1995@gmail.com>
2023-10-20 07:22:34 +0300

    created login and signup methods

--- recipe-project/blueprints/auth.py
+++ recipe-project/blueprints/auth.py
@@ -1,20 +1,111 @@
 #!/usr/bin/python3
 """Module to fetch user information """
+import scrypt
 from flask import *
+import re  # regular expression import
+from werkzeug.security import check_password_hash, generate_password_hash
+from flask_login import current_user, login_user, logout_user
 
+from models.user import User
+from models.database import db
 auth = Blueprint('auth', __name__)
-
 
 
 @auth.route('/login')
 def login():
-    """function to perform login"""
-    return render_template('/user/login.html')
+    """login method"""
+    # Render the login form if it's a GET request
+    return render_template('user/login.html')
 
 
-@auth.route('/signup')
+@auth.route('/login', methods=['GET','POST'])
+def login_post():
+    """Function to perform login"""
+    # Fetch data from the form
+    email = request.form.get('email')
+    password = request.form.get('password')
+    remember = True if request.form.get('remember') else False
+
+    # Check if the user already exists
+    user = User.query.filter_by(email = email).first()
+    print(f'{user.email}, {user.password}, {user.user_id}')
+    if not user:
+        print('You are not registered. Please sign up now.')
+        return redirect(url_for('auth.signup'))  # Redirect to the signup page if the user doesn't exist
+    elif not scrypt.hash(user.password, password) != user.password:
+        print('Please check your login details and try again.')
+        # return redirect(url_for('auth.login'))  # Redirect to the login page if the password is incorrect
+    else:
+        login_user(user, remember=remember)  # Log in the user
+        print('Authenticated')
+        # return redirect(url_for('main.index'))  # Redirect to the main page after successful login
+
+
+
+@auth.route('/signup', methods=['GET', 'POST'])
 def signup():
     """Function to perform signup of users"""
-    return render_template('/user/signup.html')
+    if request.method == 'POST':
+        name = request.form.get('name')
+        email = request.form.get('email')
+        password = request.form.get('password')
+        confirm_password = request.form.get('confirm-password')
+
+        # Password validations
+
+        # Check if the entered password matches the confirmation password
+        if password != confirm_password:
+            flash("Passwords do not match")
+            return redirect(url_for('auth.signup'))
+
+        # Check if the password length is at least 8 characters
+        if len(password) < 8 or len(confirm_password) < 8:
+            flash("Password must be at least 8 characters long.")
+            return redirect(url_for('auth.signup'))
+
+        # Check if the password contains at least one lowercase and one uppercase letter
+        if not re.search(r'[a-z]', password) or not re.search(r'[A-Z]', password):
+            flash("Password must contain both uppercase and lowercase characters.")
+            return redirect(url_for('auth.signup'))
+
+        # Check if the password contains at least one digit
+        if not re.search(r'\d', password):
+            flash("Password must contain at least one digit.")
+            return redirect(url_for('auth.signup'))
+
+        # Check if the password contains at least one special character
+        if not re.search(r'[!@#$%^&*()_+{}[\]:;<>,.?~\\-]', password):
+            flash("Password must contain at least one special character.")
+            return redirect(url_for('auth.signup'))
+
+        # Email validation
+
+        # Check if the entered email is in a valid format using a regular expression
+        # if not re.match(r"[^@]+@[^@]+\.[^@]+", email):
+        #     flash("Invalid email address.")
+        #     return redirect(url_for('auth.signup'))
+
+        # Check if the email is already in use
+        user = User.query.filter_by(email=email).first()
+        if user:
+            flash("Email address already taken.")
+            return redirect(url_for('auth.signup'))
+
+        # Create a new user and add them to the database
+        new_user = User(name=name, email=email, password=generate_password_hash(password, method='scrypt'))
+
+        db.session.add(new_user)
+        db.session.commit()
+        flash("Account created successfully! You can log in below.")
+        return redirect(url_for('auth.login'))
+
+    # Render the signup form if it's a GET request
+    return render_template('user/signup.html')
 
 
+
+@auth.route('/logout')
+def logout():
+    logout_user()
+    session.clear()
+    return redirect(url_for('auth.login'))
